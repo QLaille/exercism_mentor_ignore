d9f1da216072e186c8f03dbc43c7a9c95cbee627
QLaille <quentin.laille@epitech.eu>
2019-02-05 09:48:53 +0100

    Create README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# exercism_mentor_ignore
+Small bash script to manage the ignore lines of exercism tests.
+
+# Usage
+`./exercism_mentor_ignore.sh $function $filepath`
+Where $function is either `comment` or `uncomment`,
+and $filepath the path to the test file to modify.
--- exercism_mentor_ignore.sh
+++ exercism_mentor_ignore.sh
@@ -6,13 +6,18 @@
 
 COMMENTTYPE1='\/\/'
 COMMENTTYPE2='\#'
+
 #Feel free to add more comment type if necessary.
+
+C_TEST_IGNORE='TEST_IGNORE'
+
+#Above is the list of function calls to ignore a test, in c, it is TEST_IGNORE().
 
 function comment() {
 	case $1 in
 		*.c)
-			sed -i "/TEST_IGNORE/s/^/$COMMENTTYPE1/g" $1 ;;
-		#add between the hashtags in this switch-case your file format, and copy-paste the above sed command with the appropriated comment type.
+			sed -i "/$C_TEST_IGNORE/s/^/$COMMENTTYPE1/g" $1 ;;
+		#add between the hashtags your file format, and copy-paste the above sed command with the appropriated comment type.
 		#
 		*)
 			echo "not a valid file" >> /dev/stderr;;
@@ -22,8 +27,8 @@ function comment() {
 function uncomment() {
 	case $1 in
 		*.c)
-			sed -i "/TEST_IGNORE/s/^$COMMENTTYPE1*//g" $1;;
-		#add between the hashtags in this switch-case your file format, and copy-paste the above sed command with the appropriated comment type.
+			sed -i "/$C_TEST_IGNORE/s/^$COMMENTTYPE1*//g" $1;;
+		#add between the hashtags your file format, and copy-paste the above sed command with the appropriated comment type.
 		#
 		*)
 			echo "not a valid file" >> /dev/stderr;;
